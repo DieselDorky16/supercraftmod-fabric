6b596f18277ba9b8d327345e3965342173ef2d07
DieselDorky16 <christopher.lewis@gorhamschools.org>
2020-08-14 15:46:27 -0400

    Initial commit

---
+++ src/main/java/com/christopher/supercraft/Supercraft.java
@@ -0,0 +1,11 @@
+package com.christopher.supercraft;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Supercraft implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/com/christopher/supercraft/registry/ModItems.java
@@ -0,0 +1,4 @@
+package com.christopher.supercraft.registry;
+
+public class ModItems {
+}
